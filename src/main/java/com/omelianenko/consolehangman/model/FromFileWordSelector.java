e98547fd1a9138862a7d8605484b14bc73a2f965
y.s.omelyanenko <y.s.omelyanenko@tbank.ru>
2026-04-14 16:44:05 +0300

    separated the resource read method to util class

--- src/main/java/com/omelianenko/consolehangman/model/FromFileWordSelector.java
+++ src/main/java/com/omelianenko/consolehangman/model/FromFileWordSelector.java
@@ -1,9 +1,6 @@
 package com.omelianenko.consolehangman.model;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.omelianenko.consolehangman.util.ResourceReader;
 import java.util.List;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
@@ -16,23 +13,7 @@ public class FromFileWordSelector implements WordSelector {
 
     @Override
     public String getRandomWord() {
-        try (InputStream inputStream = getClass().getClassLoader()
-            .getResourceAsStream(resourcePath)) {
-            if (inputStream == null) {
-                throw new RuntimeException("Файл не найден в ресурсах: " + resourcePath);
-            }
-
-            List<String> words = new BufferedReader(new InputStreamReader(inputStream))
-                .lines()
-                .filter(line -> !line.trim().isEmpty())
-                .toList();
-            if (words.isEmpty()) {
-                throw new RuntimeException("Файл пуст: " + resourcePath);
-            }
-            return words.get(random.nextInt(words.size()));
-
-        } catch (IOException e) {
-            throw new RuntimeException("Ошибка чтения файла: " + resourcePath, e);
-        }
+        List<String> words = ResourceReader.readFromResource(resourcePath);
+        return words.get(random.nextInt(words.size()));
     }
 }
